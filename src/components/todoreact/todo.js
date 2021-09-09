59c74de4f8f73adc5e590b66eadf45d8107dab2b
Baberbao <baberali7732453@gmail.com>
2021-09-09 15:22:34 +0500

    message

--- src/components/todoreact/todo.js
+++ src/components/todoreact/todo.js
@@ -69,56 +69,55 @@ const Todo = () => {
 
   return (
     <>
-      <div className="main-div">
-        <div className="child-div">
-          <figure>
-            <img src="./images/todo.svg" alt="todologo" />
-            <figcaption>Add Your List Here ✌</figcaption>
-          </figure>
-          <div className="addItems">
-            <input
-              type="text"
-              placeholder="✍ Add Item"
-              className="form-control"
-              value={inputdata}
-              onChange={(event) => setInputData(event.target.value)}
-            />
-            {toggleButton ? (
-              <i className="far fa-edit add-btn" onClick={addItem}></i>
-            ) : (
-              <i className="fa fa-plus add-btn" onClick={addItem}></i>
-            )}
-          </div>
-          {/* show our items  */}
-          <div className="showItems">
-            {items.map((curElem) => {
-              return (
-                <div className="eachItem" key={curElem.id}>
-                  <h3>{curElem.name}</h3>
-                  <div className="todo-btn">
-                    <i
+      <div className="border_div">
+        <div className="main-div">
+          <div className="child-div">
+            <h1>ToDo App</h1>
+            <div className="addItems">
+              <input
+                type="text"
+                placeholder="Add your new Todo"
+                className="form-control"
+                value={inputdata}
+                onChange={(event) => setInputData(event.target.value)}
+              />
+              {toggleButton ? (
+                <i className="far fa-edit add-btn" onClick={addItem}></i>
+              ) : (
+                <i className="fa fa-plus add-btn" onClick={addItem}></i>
+              )}
+            </div>
+            {/* show our items  */}
+            <div className="showItems">
+              {items.map((curElem) => {
+                return (
+                  <div className="eachItem" key={curElem.id}>
+                    <h3>{curElem.name}</h3>
+                    <div className="todo-btn">
+                      {/* <i
                       className="far fa-edit add-btn"
                       onClick={() => editItem(curElem.id)}
-                    ></i>
-                    <i
-                      className="far fa-trash-alt add-btn"
-                      onClick={() => deleteItem(curElem.id)}
-                    ></i>
+                    ></i> */}
+                      <i
+                        className="far fa-trash-alt add-btn"
+                        onClick={() => deleteItem(curElem.id)}
+                      ></i>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-          </div>
+                );
+              })}
+            </div>
 
-          {/* rmeove all button  */}
-          <div className="showItems">
-            <button
-              className="btn effect04"
-              data-sm-link-text="Remove All"
-              onClick={removeAll}
-            >
-              <span> CHECK LIST</span>
-            </button>
+            {/* rmeove all button  */}
+            <div className="showItems">
+              <button
+                className="btn effect04"
+                data-sm-link-text="Remove All"
+                onClick={removeAll}
+              >
+                <span> CHECK LIST</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
